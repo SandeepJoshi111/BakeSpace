b9c184508d61271f70f4c7551117f10a8a362ccf
SandeepJoshi <joshisandip111@gmail.com>
2023-08-28 12:03:28 +0545

    add product

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\FrontendController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -27,11 +29,19 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::middleware(['auth','isAdmin'])->group(function () {
 
     Route::get('/dashboard',[FrontendController::class, 'index']);
+
+    // CATEGORIES
     Route::get('/categories',[CategoryController::class, 'index']);
     Route::get('add-category',[CategoryController::class, 'add']);
     Route::post('insert-category',[CategoryController::class,'insert']);
-    Route::get('edit-prod/{id}',[CategoryController::class,'edit']);
+    Route::get('edit-category/{id}',[CategoryController::class,'edit']);
     Route::put('update-category/{id}',[CategoryController::class,'update']);
     Route::get('delete-category/{id}',[CategoryController::class,'destroy']);
     
+    // PRODUCTS
+    Route::get('products',[ProductController::class,'index']);
+    Route::get('add-products',[ProductController::class,'add']);
+    Route::post('insert-product',[ProductController::class,'insert']);
+
+
  });
